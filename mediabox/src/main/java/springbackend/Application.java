5317038da8c49767de7436c998de8a530ccff58e
Daniel Walker <danieltrwalker@gmail.com>
2017-11-28 07:05:23 -0800

    Fixed errors in pom.xml

--- mediabox/src/main/java/springbackend/Application.java
+++ mediabox/src/main/java/springbackend/Application.java
@@ -1,9 +1,12 @@
 package springbackend;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args)
+        SpringApplication.run(Application.class, args);
     }
 }
