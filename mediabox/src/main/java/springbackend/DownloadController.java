36024e42f111ca9f06bc6c09fb129106bb99e815
Daniel Walker <danieltrwalker@gmail.com>
2017-11-28 10:20:45 -0800

    modified folder structure, can now serve rect-gui.html with spring

--- mediabox/src/main/java/springbackend/DownloadController.java
+++ mediabox/src/main/java/springbackend/DownloadController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class DownloadController {
 
-    @RequestMapping("/")
+    @RequestMapping({"/","/home"})
     public String download(){
         return "react-gui";
     }
