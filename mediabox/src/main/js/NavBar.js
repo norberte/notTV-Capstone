ff8fb014af3c2221d12ddd45a9e0c196ce47a53f
nick <nick@root.root>
2018-01-16 10:19:53 -0800

    The navbar is aligned correctly with each page and has css to make it look pretty. The Acordion menu in browse is broken and does not open anymore. I do not know why this happened. Probably some missing css in browse.css, but I don't know what it is that is missing.

--- mediabox/src/main/js/NavBar.js
+++ mediabox/src/main/js/NavBar.js
@@ -4,20 +4,14 @@ const ReactDOM = require("react-dom");
 export default class NavBar extends React.Component {
 render(){
   return(
-  <div>
-      <nav className="navbar navbar-default navigation-clean">
-          <div className="container">
-            <div className="navbar-header">
-              <a className="navbar-brand navbar-link" href="http://localhost:8080/browse"><img className="notTVLogo" src="/img/notTV_logo_white.png" alt="notTV Logo"/></a>
-            </div>
-              <div className="collapse navbar-collapse" id="navcol-1">
-                  <ul className="nav navbar-nav navbar-right">
-                      <li className="active" role="presentation"><a href="http://localhost:8080/browse">Watch </a></li>
-                      <li role="presentation"><a href="http://localhost:8080/upload">Submit Video</a></li>
-                      <li className="dropdown"><a className="dropdown-toggle" data-toggle="dropdown" aria-expanded="false" href="#">Account </a></li>
-                  </ul>
-              </div>
-          </div>
+  <div id="navbar">
+      <nav>
+        <a href="http://localhost:8080/browse"><img className="notTVLogo" src="/img/notTV_logo_white.png" alt="notTV Logo"/></a>
+            <ul>
+                <li><a href="http://localhost:8080/browse">Watch </a></li>
+                <li><a href="http://localhost:8080/upload">Submit Video</a></li>
+                <li><a href="#">Account </a></li>
+            </ul>
       </nav>
   </div>
   );
