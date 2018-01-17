280f80710d96dab90f81a2dff1a690c48c4b38b2
Daniel Walker <danieltrwalker@gmail.com>
2018-01-17 11:07:50 -0800

    more work on server stuff, able to return basic video data

--- server/src/main/java/spring/VideoData.java
+++ server/src/main/java/spring/VideoData.java
@@ -2,5 +2,17 @@ package spring;
 
 public class VideoData {
 
-    private final String 
+    private final long id;
+    public final String title;
+    
+    public VideoData(long id, String title){
+        this.id = id;
+        this.title = title;
+    }
+    public long getId(){
+        return id;
+    }
+    public String getTitle(){
+        return title;
+    }
 }
