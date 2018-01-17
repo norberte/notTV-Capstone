062fec23c4530955c80a2ed05536a3e480b459ac
Daniel Walker <danieltrwalker@gmail.com>
2018-01-17 13:52:18 -0800

    added database connection code to InfoController

--- server/src/main/java/spring/InfoController.java
+++ server/src/main/java/spring/InfoController.java
@@ -5,10 +5,11 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,41 +22,55 @@ import spring.view.Video;
 @RequestMapping("/info")
 public class InfoController {
     private static final Logger log = LoggerFactory.getLogger(InfoController.class);
-    
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     @GetMapping("/categories")
     @ResponseBody
     public CategoryType[] getCategories() {
-	log.info("categories");
-	// test data.
-	// CategoryValue(id, name)
-	// CategoryType(name, CategoryValue[] values)
-	// If arrays are a pain and Lists are more convenient, feel free to change the view object.
-	// I'm pretty sure it will Serialize to the same json.
-	List<CategoryValue> misc = Arrays.asList(
-	    new CategoryValue(1, "In Library"),
-	    new CategoryValue(2, "Short Videos"),
-	    new CategoryValue(3, "Long Videos")
-	);
-	List<CategoryValue> city = Arrays.asList(
-	    new CategoryValue(1, "Edmonton"),
-	    new CategoryValue(2, "Kelowna")
-	); 
-	List<CategoryType> categories = Arrays.asList(
-	    new CategoryType("Misc", misc.toArray(new CategoryValue[misc.size()])),
-	    new CategoryType("City", city.toArray(new CategoryValue[city.size()]))
-	);
-	return categories.toArray(new CategoryType[categories.size()]);
+        log.info("categories");
+        // test data.
+        // CategoryValue(id, name)
+        // CategoryType(name, CategoryValue[] values)
+        // If arrays are a pain and Lists are more convenient, feel free to
+        // change the view object.
+        // I'm pretty sure it will Serialize to the same json.
+        List<CategoryValue> misc = Arrays.asList(new CategoryValue(1, "In Library"),
+                new CategoryValue(2, "Short Videos"), new CategoryValue(3, "Long Videos"));
+        List<CategoryValue> city = Arrays.asList(new CategoryValue(1, "Edmonton"), new CategoryValue(2, "Kelowna"));
+        List<CategoryType> categories = Arrays.asList(
+                new CategoryType("Misc", misc.toArray(new CategoryValue[misc.size()])),
+                new CategoryType("City", city.toArray(new CategoryValue[city.size()])));
+        return categories.toArray(new CategoryType[categories.size()]);
     }
 
     @GetMapping("/videos")
     @ResponseBody
-    public Video[] getVideos(@RequestParam(value="categories[]") int[] categories) {
+    public Video[] getVideos() {
 	// Video(title, thumbnail_url, download_url)
 	// I don't think we store thumbnails yet, so it's okay to just use the placeholder for now.
 	// For the download url, I think we just store the torrent file(?) so just append
 	// the name of the torrent to /download?torrentName=
-	log.info("videos");
-	log.info(Arrays.toString(categories));
+        
+    String query = "Select * From Video";
+    /*
+    if(filter != null){
+        for(String categoryValue :filter.split("+")){
+            query += "Intersect +"
+                      + "Select categoryValueId"
+                      + "From video_category_value_join Natural Join category_value As cv"
+                      + "Where cv.name = " + categoryValue;
+        }
+    }
+    */
+    query += ";";
+            
+    List<Video> videos = jdbcTemplate.query(query, 
+            (rs, row) -> new Video(rs.getString("title"), 
+                        "/img/default-placeholder-300x300.png",
+                        "/download?torrentName="+rs.getString("url")) ); 
+    /* 
 	List<Video> videos = Arrays.asList(
 	    new Video(
 		"Title",
@@ -63,6 +78,7 @@ public class InfoController {
 		"/download?torrentName=test.torrent"
 	    )
 	);
+	*/
 	return videos.toArray(new Video[videos.size()]);
     }
 }
