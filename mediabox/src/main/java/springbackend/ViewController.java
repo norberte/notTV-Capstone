2791fd59b662b52237320ab1bdb2efe98b832ab4
Levi Miller <levi@levimiller.ca>
2018-01-22 17:38:24 -0800

    Generalized the template for the browse page.

--- mediabox/src/main/java/springbackend/ViewController.java
+++ mediabox/src/main/java/springbackend/ViewController.java
@@ -21,6 +21,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -79,9 +80,11 @@ public class ViewController {
 	return "player";
     }
     
-    @RequestMapping("browse")
-    public String browse() {
-	return "browse";
+    @RequestMapping("{page}")
+    public String defaultPage(@PathVariable String page, Model model) {
+	model.addAttribute("title", StringUtils.capitalize(page));
+	model.addAttribute("page_name", page);
+	return "default_page";
     }
 
     @RequestMapping("upload")
