ade5a00742d8df9e2db92ffe276a3ab64a9d8226
Levi Miller <levi@levimiller.ca>
2018-03-06 00:23:55 -0800

    rebased with develop

--- mediabox/src/main/js/upload/Upload.js
+++ mediabox/src/main/js/upload/Upload.js
@@ -4,6 +4,14 @@ import BreadCrumb from './BreadCrumb.js';
 const React = require("react");
 const ReactDOM = require("react-dom");
 
+function toCamel(str) {
+    const name = str.replace(" ", "");
+    return name.charAt(0).toLowerCase() + name.slice(1);
+}
+
+// gets the categories from the server and handles the user selecting them.
+// args: data - dictionary with tags Set
+// setTags - method to call to update the tags.
 class CategoryPane extends React.Component {
     constructor(props) {
         super(props);
@@ -126,41 +134,66 @@ class EssentialPane extends React.Component {
     }
 }
 
-class ImgFilePane extends React.Component {
+
+// Buttons for navigating the UploadForm. These handle the logic of disabling themselves.
+// args: curr - current position, length: total number of positions
+// handleSubmit - what to do upon submission, increment - what to call to change position.
+class NextButton extends React.Component {
     render() {
-	return (
-	      <div className="tab">
-		<h1>{this.props.label}</h1>
-		<p>
-		  <input type="file" name="videoThumbnail" accept="image/*"
-			 ref={(input) => { this.videoThumbnail = input; }} onChange={()=>this.props.onChange(this.videoThumbnail.files)}/>
-		</p>
-	      </div>
-	);
+        if(this.props.curr === this.props.length - 1) {
+            return (
+                <li className="next submit">
+                  <a onClick={this.props.submit}>
+                    Submit
+                  </a>
+                </li>
+            );
+        }
+        return (
+            <li className="next">
+              <a onClick={()=>this.props.increment(1)}>
+                Continue <span aria-hidden="true">&rarr;</span>
+              </a>
+            </li>
+        );
     }
 }
 
-class App extends React.Component {
-    constructor(props){
-	super(props);
-	this.state = {
-	    videoFile: null,
-	    videoThumbnail: null,
-	    formData: {
+// args: increment - change position, curr - current position.
+class PrevButton extends React.Component {
+    render() {
+        const disable = this.props.curr === 0 ? " disabled" : "";
+        return (
+            <li className={"previous" + disable}>
+              <a onClick={()=>this.props.increment(-1)}>
+                <span aria-hidden="true">&larr;</span> Go Back
+              </a>
+            </li>
+        );
+    }
+}
+
+class UploadForm extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            curr: 0,
+            formData: {
+                videoFile: null,
+	        thumbnailFile: null,
 		title: '',
 		description: '',
 		version: '',
 		license: '',
-		thumbnailurl: '/img/default-placeholder-300x300.png', // this is the default video thumbnail... if user uploads an actual thumbnail, the backend will overwrite the default
-		tags: '',
-		userid: '-1' // TODO: get mediabox user id.
-	    }
-	};
+		tags: new Set(),
+		userid: '-1' // TODO: mediabox user id.
+            }
+        };
 
         this.increment = this.increment.bind(this);
 	this.handleChange = this.handleChange.bind(this);
 	this.fileChange = this.fileChange.bind(this);
-	this.imgFileChange = this.imgFileChange.bind(this);
 	this.handleSubmit = this.handleSubmit.bind(this);
         this.setTags = this.setTags.bind(this);
     }
@@ -202,209 +235,59 @@ class App extends React.Component {
             }
         };
     }
-    
-    imgFileChange(files) {
-    	if(files.length > 0) // Only one file for now.
-    	    this.state.videoThumbnail = files[0];
-    }
-    
+
     //handles getting state data and giving it to the ajax submit.
     handleSubmit(e){
-    	e.preventDefault();
-    	
-    	// initialize a "global" thumbnailURL with default value that each post request can access
-    	var thumbnailURL = this.state.formData.thumbnailurl;
-    	
-    	const localForm = new FormData();
-    	localForm.append('video', this.state.videoFile);
-    	
-    	// Start upload process on local mediabox server.
-    	$.post({
-    		url: '/process/upload',
-    		data: localForm,
-    		processData: false,  // tell jQuery not to process the data (because of the file)
-    		contentType: false,  // tell jQuery not to set contentType
-    		success: (torrentFile) => {
-    			// make FormData object to store thumbnail image to be uploaded
-    	    	const newForm = new FormData();
-    	    	newForm.append('image', this.state.videoThumbnail);
+	e.preventDefault();
 
-    	    	// post request to upload thumbnail to server
-    	        $.post({
-    	    		url: config.serverUrl + '/upload/thumbnailSubmission',
-    	    		data: newForm,
-    	    		processData: false,  // not to process the data (because of the file)
-    	    		contentType: false,  // not to set contentType
-    	    		success: (thumbnailURL_returned) => {
-    	    			// change thumnailURl, if it was uploaded to server, else, leave it as default value
-    	    			if(thumbnailURL_returned !== ""){
-    	    				thumbnailURL = thumbnailURL_returned.substring(thumbnailURL_returned.indexOf("/img/"));
-    	    				console.log("Thumbnail Submission returned an actual THUMBNAIL URL: " + thumbnailURL_returned);
-    	    			} else {
-    	    				console.log("Thumbnail Submission returned empty String!");
-    	    				// keep thumbnailURL to the default picture
-    	    			}
-    	    			
-    	    			// ajax request to send video metadata to server
-    	    			// TODO: loop through formData <-- someone else's comment .. not sure why this is needed
-    	    			const formData = {
-    	    					title: this.state.formData.title,
-    	    					description: this.state.formData.description,
-    	    					version: this.state.formData.version,
-    	    					filetype: this.state.videoFile.type,
-    	    					license: this.state.formData.license,
-    	    					downloadurl: torrentFile,
-    	    					thumbnailurl: thumbnailURL,
-    	    					tags: this.state.formData.tags,
-    	    					userid: this.state.formData.userid
-    	    			};
+	const localForm = new FormData();
+	localForm.append('video', this.state.formData.videoFile);
+	// Start upload process on local mediabox server.
+	$.post({
+	    url: '/process/upload',
+	    data: localForm,
+	    processData: false,  // tell jQuery not to process the data (because of the file)
+	    contentType: false,  // tell jQuery not to set contentType
+	    success: (torrentFile) => {
+		// TODO: loop through formData
+                console.log("local success");
+		const formData = {
+		    title: this.state.formData.title,
+		    description: this.state.formData.description,
+		    version: this.state.formData.version,
+		    license: this.state.formData.license,
+		    downloadurl: torrentFile,
+                    thumbnail: this.state.formData.thumbnailFile,
+		    tags: this.state.formData.tags,
+		    userid: this.state.formData.userid
+		};
 
-    	    			// insert video-metadata
-    	    			$.ajax({
-    	    				type: "POST",
-    	    				url: config.serverUrl + "/upload/videoSubmission",
-    	    				contentType: 'application/json',
-    	    				processData: false,
-    	    				data: JSON.stringify(formData),
-    	    				success: (response) => {
-    	    					console.log(response);
-    	    					alert("Successfully uploaded!");
-    	    				},
-    	    				error: (response) => {
-    	    					console.log(response);
-    	    				}
-    	    			});
-    	    		},
-    	    		error: (response) => {
-    	    			console.log("Thumbnail Submission DID NOT WORK!");
-    	    		}
-    	        });
-    		},
-    		error: (response) => {
-    			console.log(response);
-    		}
-    	});
+		// insert video
+		$.ajax({
+		    type: "POST",
+		    url: config.serverUrl + "/upload/videoSubmission",
+		    contentType: 'application/json',
+		    processData: false,
+		    data: JSON.stringify(formData),
+		    success: (response) => {
+			console.log(response);
+			BootstrapDialog.show({
+                            title: "Success!",
+                            message: "Successfully uploaded!",
+                            type: BootstrapDialog.TYPE_SUCCESS
+                        });
+		    },
+		    error: (response) => {
+			console.log(response);
+		    }
+		});
+	    },
+	    error: (response) => {
+		console.log(response);
+	    }
+	});
     }
-
-    render(){
-	return(
-	    <div id="formDiv">
-	      <form id="uploadVideo" method="post" onSubmit={this.handleSubmit} commandname="videoForm">
-		<fieldset>
-		  <Tabs>
-
-		      <FilePane label="Select Video File" onChange={this.fileChange}/>
-		      <ImgFilePane label="Select Video Thumbnail" onChange={this.imgFileChange}/>
-
-		    <Pane label="Add Video Details">
-		      <div className="tab">
-			<h1>Video Details</h1>
-			<h2>Video Title</h2>
-			<input type = "text" name="title" value={this.state.formData.title}  onChange={this.handleChange}/>
-
-			<h2>Description</h2>
-			<textarea rows="4" cols="50" name="description" value={this.state.formData.description}  onChange={this.handleChange}>
-			  Enter the description of the video here.
-			</textarea>
-
-			<h2>Tags</h2>
-			<textarea rows="4" cols="50" name="tags" value={this.state.formData.tags}  onChange={this.handleChange}>
-			  Enter keywords seperated by a comma. For example: Edmonton,Winter,Driving,Fast
-			</textarea>
-		      </div>
-		    </Pane>
-
-		    <Pane label="More Video Details">
-		      <div className="tab">
-			<h2>License</h2>
-			Allow adaptations of this work to be shared?<br/>
-			<input type="radio" name="license" value="Y" checked={this.state.formData.license === 'Y'}  onChange={this.handleChange}/>Yes&emsp;
-			<input type="radio" name="license" value="N" checked={this.state.formData.license === 'N'}  onChange={this.handleChange}/>No&emsp;
-			<input type="radio" name="license" value="SA" checked={this.state.formData.license === 'SA'}  onChange={this.handleChange}/>Yes, as long as others share alike
-
-			<h2>Version</h2>
-			<input type = "number" name="version" value={this.state.formData.version}  onChange={this.handleChange}/>
-		      </div>
-		    </Pane>
-
-		    <Pane label="Submit Video">
-		      <div className="tab">
-			<h1>Submit Video</h1>
-			<input type="hidden" name="author" value="-1"  onChange={this.handleChange}/>
-			<input type="submit" value="Submit"/>
-		      </div>
-		    </Pane>
-		  </Tabs>
-		</fieldset>
-	      </form>
-	    </div>
-	);
-    }
-}
-
-/*
- A containter that iterates through a list of React Components.
-*/
-class UploadContainer extends React.Component {
-    render() {
-        const disableNext = this.props.curr === this.props.items.length - 1 ? " disabled" : "";
-        const disablePrev = this.props.curr === 0 ? " disabled" : "";
-        return (
-            <div className="row">
-              <div className="col-md-12 upload-container">
-                <div className="row">
-                  {this.props.items[this.props.curr]}
-                </div>
-
-                <div className="row">
-                  <ul className="pager">
-                    <li className={"previous" + disablePrev}>
-                      <a onClick={()=>this.props.increment(-1)}>
-                        <span aria-hidden="true">&larr;</span> Go Back
-                      </a>
-                    </li>
-                    <li className={"next" + disableNext}>
-                      <a onClick={()=>this.props.increment(1)}>
-                        Continue <span aria-hidden="true">&rarr;</span>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-        );
-    }
-}
-
-class Test extends React.Component {
-    render() {
-        return (
-            <div>
-              {this.props.content}
-            </div>
-        );
-    }
-}
-
-class UploadForm extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            curr: 0
-        };
-
-        this.increment = this.increment.bind(this);
-    }
-
-    increment(amount) {
-        const newVal = this.state.curr + amount;
-
-        if(newVal >=0 && newVal < this.props.items.length)
-            this.setState({
-                curr: newVal
-            });
-    }
+    
     render() {
         const CompClass = this.props.items[this.state.curr];
         return (
