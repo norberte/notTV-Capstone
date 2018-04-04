605f666fe3130df2bb94332e9c72a71601c4fcec
Levi Miller <levi@levimiller.ca>
2018-04-04 10:31:57 -0700

    Fixed som layout issues, and the user profile not showing up.

--- mediabox/src/main/js/account/Account.js
+++ mediabox/src/main/js/account/Account.js
@@ -10,7 +10,6 @@ const ReactDOM = require("react-dom");
 
 let default_loggedIn_userID = -1;
 let default_loggedIn_username = 'default_user';
-let thumbnailURL;
 class UserThumbnail extends React.Component {
     constructor(props) {
         super(props);
@@ -22,15 +21,15 @@ class UserThumbnail extends React.Component {
     /*
      * User Profile Picture download is not implemented yet
      * 
-    getThumbnail() {
-        // get thumbnail if it isn't loaded already.
-        if(!this.props.entry.thumbnailURL)
-            utils.getThumbnail(this.props.entry.id, (file) => {
-                this.props.entry.thumbnailURL = file;
-                this.forceUpdate();
-            });
-    }
-    */
+     getThumbnail() {
+     // get thumbnail if it isn't loaded already.
+     if(!this.props.entry.thumbnailURL)
+     utils.getThumbnail(this.props.entry.id, (file) => {
+     this.props.entry.thumbnailURL = file;
+     this.forceUpdate();
+     });
+     }
+     */
 
     render() {
         // need to call here because the constructor isn't always called for entries.
@@ -38,20 +37,19 @@ class UserThumbnail extends React.Component {
         // We need to do it in the constructor also, because the component needs
         // to be mounted first to forceUpdate().
         //this.getThumbnail();
-        const thumbnail = this.props.entry.thumbnailURL ? this.props.entry.thumbnailURL : "/img/default-placeholder-300x300.png";
-        thumbnailURL = thumbnail;
+        const thumbnail = "/img/default-placeholder-300x300.png";
         return (
             <div className="col-md-2 no-padding">
-            <a href={this.props.entry.userProfileURL}>
-                 <div className="thumbnail no-margin">
-                     <div className="video-thumbnail">
-                         <img className="video-thumbnail-content" src={thumbnail}/>
-                     </div>
-                     <div className="caption">
-                         <h4 className="no-margin">{this.props.entry.username} </h4>
-                     </div>
+              <a href={this.props.entry.userProfileURL}>
+                <div className="thumbnail no-margin">
+                  <div className="video-thumbnail">
+                    <img className="video-thumbnail-content" src={thumbnail}/>
+                  </div>
+                  <div className="caption">
+                    <h4 className="no-margin">{this.props.entry.username} </h4>
+                  </div>
                 </div>
-            </a>
+              </a>
             </div>
         );
     }
@@ -59,52 +57,52 @@ class UserThumbnail extends React.Component {
 
 export default class Account extends React.Component {
     constructor(props){
-    super(props);
-    this.state = {
-    	userLogin: default_loggedIn_userID,
-        videos: [],
-        subscriptions: [],
-        formData:{
+        super(props);
+        this.state = {
+    	    userLogin: default_loggedIn_userID,
+            videos: [],
+            subscriptions: [],
+            formData:{
         	userId: default_loggedIn_userID,
         	currentUsername: default_loggedIn_username,
         	newEmail: '',
         	autoDownload: '',
         	newPass: '',
         	confirmNewPass: ''
-        }
-    }
-   
+            }
+        };
+        
 	//this.update_videos = this.update_videos.bind(this);
-    this.update_subscriptions = this.update_subscriptions.bind(this);
-    this.update_videos = this.update_videos.bind(this);
-    
-    // get subscriptions
-    this.update_subscriptions();
-
-    // get videos
+        this.update_subscriptions = this.update_subscriptions.bind(this);
+        this.update_videos = this.update_videos.bind(this);
+        
+        // get subscriptions
+        this.update_subscriptions();
+        
+        // get videos
 	this.update_videos();
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+        
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     };
 
     //Get a list of subscriptions
     update_subscriptions() {
-    $.get({
-        url: config.serverUrl + "/info/subscriptions",
-        data: {
-            loggedInUserID: this.state.userLogin
-        },
-        dataType: "json",
-        success: (data) => {
-        this.setState({
-        	subscriptions: data
+        $.get({
+            url: config.serverUrl + "/info/subscriptions",
+            data: {
+                loggedInUserID: this.state.userLogin
+            },
+            dataType: "json",
+            success: (data) => {
+                this.setState({
+        	    subscriptions: data
+                });
+            },
+            error: (response) => {
+                console.log(response);
+            }
         });
-        },
-        error: (response) => {
-        console.log(response);
-        }
-    });
     }
 
     
@@ -133,76 +131,75 @@ export default class Account extends React.Component {
 
     //handles getting state data for the settings form and giving it to the ajax submit.
     handleSubmit(e){
-	       e.preventDefault();
+	e.preventDefault();
 
-           const formData = {
-               currentUsername: this.state.formData['currentUsername'],
-               newPass: this.state.formData['newPass'],
-               newEmail: this.state.formData['newEmail'],
-               confirmNewPass: this.state.formData['confirmNewPass'],
-               autoDownload: this.state.formData['autoDownload']
-   		};
+        const formData = {
+            currentUsername: this.state.formData['currentUsername'],
+            newPass: this.state.formData['newPass'],
+            newEmail: this.state.formData['newEmail'],
+            confirmNewPass: this.state.formData['confirmNewPass'],
+            autoDownload: this.state.formData['autoDownload']
+   	};
 
-           //send new account details
-           $.post({
-               url: config.serverUrl + "/upload/accountSubmit",
-               contentType: 'application/json',
-               processData: false,
-               data: JSON.stringify(formData),
-               success: (response) => {
-                   console.log(response);
-                   alert("Successfully uploaded!");
-               },
-               error: (response) => {
-                   console.log(response);
-               }
-           });
+        //send new account details
+        $.post({
+            url: config.serverUrl + "/upload/accountSubmit",
+            contentType: 'application/json',
+            processData: false,
+            data: JSON.stringify(formData),
+            success: (response) => {
+                console.log(response);
+                alert("Successfully uploaded!");
+            },
+            error: (response) => {
+                console.log(response);
+            }
+        });
     }
 
 
     render() {
-    return (
+        const thumbnail = "/img/default-placeholder-300x300.png";
+        return (
             <div id ="myContainer">
-            <div className="container">
+              <div className="container">
                 <div className="row">
-                    <div className="col-md-10 col-md-offset-1">
+                  <div className="col-md-10 col-md-offset-1">
                     <div id = 'accountInfo'>
-                        <h1>Account Info</h1>
-                        <div id = 'accountDetails'>
-                            <div id = 'NewContainer'>
-                                <figure>
-                                    <img src = {thumbnailURL} alt = "Profile Picture"/>
-                                    <figcaption>{default_loggedIn_username}</figcaption>
-                                </figure>
-                            </div>
-            
-                            <form id="accountInfoForm" method="post" onSubmit={this.handleSubmit} commandname="accountForm">
-                                <input type = "text" name="newUsername" value={this.state.formData.username}  onChange={this.handleChange} placeholder="Enter a New Username" disabled/><br />
-                                <input type = "text" name="newEmail" value={this.state.formData.email}  onChange={this.handleChange} placeholder="Enter a New Email" /><br />
-                                <input type = "password" name="newPass" value={this.state.formData.newPass}  onChange={this.handleChange} placeholder="Enter a New Password" /><br />
-                                <input type = "password" name="confirmNewPass" value={this.state.formData.confirmNewPass}  onChange={this.handleChange} placeholder="Confirm New Password" />
-            
-                                <input type="submit" value="Submit"/>
-                            </form>
-                            <form id = "hide">
-                                <input type="submit" value="Submit"/>
-                            </form>
+                      <h1>Account Info</h1>
+                      <div id = 'accountDetails'>
+                        <div id = 'NewContainer'>
+                          <figure>
+                            <img src = {thumbnail} alt = "Profile Picture"/>
+                            <figcaption>{default_loggedIn_username}</figcaption>
+                          </figure>
                         </div>
-                        <div className = "row browse-body">
-                            <CarouselLayout thumbnailClass={UserThumbnail} title="Users Subscribed To" entries={this.state.subscriptions}/>
-                        </div>
-            	        <div className = "row browse-body">
-            	            <CarouselLayout thumbnailClass={VideoThumbnail} title="In-Library Videos" entries={this.state.videos}/>
-            	        </div>
+                        
+                        <form id="accountInfoForm" method="post" onSubmit={this.handleSubmit} commandname="accountForm">
+                          <input type = "text" name="newUsername" value={this.state.formData.username}  onChange={this.handleChange} placeholder="Enter a New Username" disabled/><br />
+                          <input type = "text" name="newEmail" value={this.state.formData.email}  onChange={this.handleChange} placeholder="Enter a New Email" /><br />
+                          <input type = "password" name="newPass" value={this.state.formData.newPass}  onChange={this.handleChange} placeholder="Enter a New Password" /><br />
+                          <input type = "password" name="confirmNewPass" value={this.state.formData.confirmNewPass}  onChange={this.handleChange} placeholder="Confirm New Password" />
+                          
+                          <input type="submit" value="Submit"/>
+                        </form>
+                        <form id = "hide">
+                          <input type="submit" value="Submit"/>
+                        </form>
+                      </div>
+                      <div className = "row browse-body">
+                        <CarouselLayout thumbnailClass={UserThumbnail} title="Users Subscribed To" entries={this.state.subscriptions}/>
+                      </div>
+            	      <div className = "row browse-body">
+            	        <CarouselLayout thumbnailClass={VideoThumbnail} title="In-Library Videos" entries={this.state.videos}/>
+            	      </div>
                     </div>
-                    </div>
+                  </div>
                 </div>
-          </div>
-     </div>
-    );
+              </div>
+            </div>
+        );
     }
 }
 
-ReactDOM.render(<Account />, 
-		document.getElementById('root') 
-	);
+ReactDOM.render(<Account />, document.getElementById('root'));
